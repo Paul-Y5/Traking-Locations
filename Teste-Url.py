0a0a3134ed046683f974a07943f0e011838ba5c1
Paul-Y5 <pauladascardoso@gmail.com>
2023-12-05 15:32:26 +0000

    ...

--- Teste-Url.py
+++ Teste-Url.py
@@ -1,7 +1,7 @@
 import requests
 from requests.structures import CaseInsensitiveDict
 
-url = "https://api.geoapify.com/v2/places?categories=commercial.supermarket&filter=rect%3A10.716463143326969%2C48.755151258420966%2C10.835314015356737%2C48.680903341613316&limit=20&apiKey=3b217b38a08846b19772470894a181b7"
+url = "https://api.geoapify.com/v2/places?categories=activity&filter=rect:16.17903071551638,48.28499347053008,16.556767855560192,48.0623697740825&limit=20&apiKey=3b217b38a08846b19772470894a181b7"
 
 headers = CaseInsensitiveDict()
 headers["Accept"] = "application/json"
@@ -9,3 +9,4 @@ headers["Accept"] = "application/json"
 resp = requests.get(url, headers=headers)
 
 print(resp.status_code)
+print(resp.content)
