15ffd911c3da19ed83c21d9235a215160baca3ef
TSH-19 <tomas.santos.hilario@gmail.com>
2023-12-05 15:54:01 +0000

    ...

---
+++ tete.py
@@ -0,0 +1,12 @@
+import requests
+
+# Base URL for the API
+base_url = "https://api.geoapify.com/v2/places?categories="
+
+place = input('Place coordinates: ')
+category = input('category: ')
+kimit = input('limit: ')
+
+url = (f"{base_url}{category}&filter=rect:{place}&limit=500&apikey=3b217b38a08846b19772470894a181b7")
+response = requests.get(url)
+print(response.json())
