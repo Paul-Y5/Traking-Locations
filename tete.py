b6ea78ab4d32a2a51261fa702ed44f584fac0f67
Paul-Y5 <pauladascardoso@gmail.com>
2023-12-05 16:08:02 +0000

    url

--- tete.py
+++ tete.py
@@ -1,5 +1,5 @@
 import requests
-
+from requests.structures import CaseInsensitiveDict
 # Base URL for the API
 base_url = "https://api.geoapify.com/v2/places?categories="
 
@@ -7,6 +7,9 @@ place = input('Place coordinates: ')
 category = input('category: ')
 kimit = input('limit: ')
 
-url = (f"{base_url}{category}&filter=rect:{place}&limit=500&apikey=3b217b38a08846b19772470894a181b7")
-response = requests.get(url)
+url = (f"{base_url}{category}&filter=rect:{place}&limit={kimit}&apikey=3b217b38a08846b19772470894a181b7")
+headers = CaseInsensitiveDict()
+headers["Accept"] = "application/json"
+response = requests.get(url, headers=headers)
+print(url)
 print(response.json())
